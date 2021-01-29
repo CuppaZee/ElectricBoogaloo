151940144a676f2261971403860a2c15d9b18e9d
Sam Hindess <mail@sohcah.dev>
2021-01-29 23:20:59 +0000

    iOS Widgets

--- copy.js
+++ copy.js
@@ -1,9 +1,17 @@
 const fs = require("fs-extra");
 const path = require("path");
-// fs.rmdirSync(path.resolve(__dirname, process.argv[3]));
+try {
+fs.removeSync(path.resolve(__dirname, process.argv[3]));
+console.log('rmed');
+} catch(e){console.log('rmer', e)}
+try {
+fs.rmdirSync(path.resolve(__dirname, process.argv[3]));
+console.log('rmdired');
+} catch(e){console.log('rmdirer', e)}
 try {
 fs.copySync(path.resolve(__dirname, process.argv[2]), path.resolve(__dirname, process.argv[3]), {
   overwrite: true,
   errorOnExist: false,
 });
-} catch(e){}
+console.log('cped');
+} catch(e){console.log('cper', e)}
