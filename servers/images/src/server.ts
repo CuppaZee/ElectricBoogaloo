1f3fd0df04c2b80f69dc06791e2ca8ec2f7ee885
Sam Hindess <mail@sohcah.dev>
2021-04-17 22:39:11 +0100

    Icon Overrides

--- servers/images/src/server.ts
+++ servers/images/src/server.ts
@@ -3,14 +3,41 @@ const fastify = Fastify({ logger: true });
 import path from "path";
 import sharp from "sharp";
 import fetch from "node-fetch";
-import { createReadStream, createWriteStream, promises } from "fs";
+import { createReadStream, createWriteStream, promises, existsSync, mkdirSync } from "fs";
 import types from "@cuppazee/types";
 
-async function getImage(urls: string[]) {
+const overrideDir = path.join(__dirname, "../override");
+const cacheDir = path.join(__dirname, "../cache");
+const cacheHeaders = {
+  "Cache-Control": "public, max-age=43200, s-maxage=43200",
+  "Access-Control-Allow-Origin": "*",
+};
+
+if (!existsSync(cacheDir)) mkdirSync(cacheDir);
+
+async function getImage(type: string) {
+  const urls: string[] = [
+    !type.match(/[A-Z\s]/) &&
+      `https://munzee.global.ssl.fastly.net/images/pins/${encodeURIComponent(type)}.png`,
+    `https://munzee.global.ssl.fastly.net/images/pins/${encodeURIComponent(
+      type.toLowerCase().replace(/\s/g, "")
+    )}.png`,
+    `https://munzee.global.ssl.fastly.net/images/pins/${encodeURIComponent(
+      type.toLowerCase().replace(/\s/g, "_")
+    )}.png`,
+  ]
+    .filter(Boolean)
+    .filter((v, i, a) => a.indexOf(v) === i) as string[];
+  
+  try {
+    await promises.access(path.join(overrideDir, type + ".png"));
+    return createReadStream(path.join(overrideDir, type + ".png"));
+  } catch (e) {}
+
   for (var url of urls) {
     const response = await fetch(url);
     if (response.ok) {
-      return response;
+      return response.body;
     }
   }
   return null;
@@ -19,6 +46,7 @@ async function getImage(urls: string[]) {
 type Format = "png" | "jpeg";
 
 fastify.get("/types/:size/:type", async function (request, reply) {
+  // Parse and Validate Parameters
   const params = request.params as {
     size: string;
     type: string;
@@ -32,62 +60,40 @@ fastify.get("/types/:size/:type", async function (request, reply) {
   type = types.getType(type)?.icon ?? type;
   const format = params.type.split(".").slice(-1)[0] as Format;
   if (format !== "jpeg" && format !== "png") {
-    reply.send(`Invalid Size: ${size}. Size >0, <=512`);
+    reply.send(`Invalid Format: ${format}. Must be jpeg or png.`);
     return;
   }
+
+  const cacheFilePath = path.join(cacheDir, `${size.toString()}_${type}.${format}`);
 
   try {
-    await promises.access(path.join(__dirname, "../icons", `${size.toString()}_${type}.${format}`));
-    reply
-      .headers({
-        "Cache-Control": "public, max-age=43200, s-maxage=43200",
-        "Access-Control-Allow-Origin": "*",
-      })
-      .type(`image/${format}`)
-      .send(
-        createReadStream(path.join(__dirname, "../icons", `${size.toString()}_${type}.${format}`))
-      );
+    // Already Cached
+    await promises.access(cacheFilePath);
+    reply.headers(cacheHeaders).type(`image/${format}`).send(createReadStream(cacheFilePath));
     return;
-  } catch(e){}
+  } catch (e) {}
 
-  const urls: string[] = [
-    !type.match(/[A-Z\s]/) && `https://munzee.global.ssl.fastly.net/images/pins/${encodeURIComponent(type)}.png`,
-    !type.match(/[A-Z\s]/) && `https://munzee.global.ssl.fastly.net/images/pins/${encodeURIComponent(type.toLowerCase().replace(/\s/g, ""))}.png`,
-    !type.match(/[A-Z\s]/) && `https://munzee.global.ssl.fastly.net/images/pins/${encodeURIComponent(type.toLowerCase().replace(/\s/g, "_"))}.png`,
-  ].filter(Boolean).filter((v,i,a) => a.indexOf(v) === i) as string[];
+  const response = await getImage(type);
 
-  const response = await getImage(urls);
-
-  if (response) {
-    const transformer = sharp()
-      .resize(size)
-      [format]();
-    const stream = response.body.pipe(transformer);
-    stream
-      .clone()
-      .pipe(createWriteStream(path.join(__dirname, "../icons", `${size.toString()}_${type}.${format}`)));
+  if (!response) {
+    // No Image Found
     reply
-      .headers({
-        "Cache-Control": "public, max-age=43200, s-maxage=43200",
-        "Access-Control-Allow-Origin": "*",
-      })
-      .type(`image/${format}`)
-      .send(stream);
+      .headers(cacheHeaders)
+      .type(`image/png`)
+      .send(createReadStream(path.join(overrideDir, "missing.png")));
     return;
   }
 
-  reply
-    .headers({
-      "Cache-Control": "public, max-age=43200, s-maxage=43200",
-      "Access-Control-Allow-Origin": "*",
-    })
-    .type(`image/png`)
-    .send(createReadStream(path.join(__dirname, "../icons/missing.png")));
+  // Image Found
+  const transformer = sharp().resize(size)[format]();
+  const stream = response.pipe(transformer);
+  stream.clone().pipe(createWriteStream(cacheFilePath));
+  reply.headers(cacheHeaders).type(`image/${format}`).send(stream);
 });
 
 const start = async () => {
   try {
-    await fastify.listen(80, "0.0.0.0");
+    await fastify.listen(1234);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
