ecd18414796bee2dde8568b6380ce847657e49a1
Sam Hindess <mail@sohcah.dev>
2021-07-07 21:19:25 +0000

    Bouncer Service

--- servers/main/src/bouncers/index.ts
+++ servers/main/src/bouncers/index.ts
@@ -2,4 +2,5 @@ import overview from "./overview";
 import list from "./list";
 import nearby from "./nearby";
 import expiring from "./expiring";
-export default [overview, list, nearby, expiring];
+import all from "./all";
+export default [overview, list, nearby, expiring, all];
