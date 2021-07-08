9563b9f7825c5285de986a3ece452df7e02a4ecd
Sam Hindess <mail@sohcah.dev>
2021-07-08 19:53:45 +0000

    Blog Monitor Service

--- servers/main/src/minute/index.ts
+++ servers/main/src/minute/index.ts
@@ -1,7 +1,6 @@
 import clan_list from "./clan_list";
 import shadow_clans from "./shadow_clans";
-import blog_checker from "./blog_checker";
 import notifications from "./notifications";
 import clanreq from "./clanreq";
 
-export default [clan_list, shadow_clans, blog_checker, notifications, clanreq];
+export default [clan_list, shadow_clans, notifications, clanreq];
